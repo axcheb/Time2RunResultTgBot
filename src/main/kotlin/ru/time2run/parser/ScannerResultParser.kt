1dfae2c7f5f33865bc01b15a6eb3b7e57a20bff1
Aleksey Chebotarev <chebotarev88@gmail.com>
2022-03-26 14:45:27 +0300

    Fix index out of range exception if position/athlete is empty

--- src/main/kotlin/ru/time2run/parser/ScannerResultParser.kt
+++ src/main/kotlin/ru/time2run/parser/ScannerResultParser.kt
@@ -1,13 +1,17 @@
 package ru.time2run.parser
 
+import mu.KotlinLogging
 import ru.time2run.model.ScannerResult
 import java.time.format.DateTimeParseException
+
+private val logger = KotlinLogging.logger {}
 
 fun isScannerResult(csv: String): Boolean {
     return try {
         val line = csv.lineSequence().drop(1).first()
         parseScannerLine(line) != null
     } catch (e: NoSuchElementException) {
+        logger.catching(e)
         false
     }
 }
@@ -26,7 +30,12 @@ fun parseScannerResult(csv: String): List<ScannerResult> {
 private fun parseScannerLine(line: String): ScannerResult? {
     return try {
         val parts = line.split(",")
-        if (parts.size == 3 && parts[0][0] == 'A' && parts[1][0] == 'P') {
+        if (parts.size == 3
+            && parts[0].length > 1
+            && parts[1].length > 1
+            && parts[0][0] == 'A'
+            && parts[1][0] == 'P'
+        ) {
             ScannerResult(
                 parts[0].trim().drop(1).toInt(),
                 parts[1].trim().drop(1).toInt(),
@@ -34,8 +43,10 @@ private fun parseScannerLine(line: String): ScannerResult? {
             )
         } else null
     } catch (e: NumberFormatException) {
+        logger.catching(e)
         null
     } catch (e: DateTimeParseException) {
+        logger.catching(e)
         null
     }
 }
--- src/test/kotlin/ru/time2run/parser/ScannerResultParserKtTest.kt
+++ src/test/kotlin/ru/time2run/parser/ScannerResultParserKtTest.kt
@@ -10,7 +10,9 @@ internal class ScannerResultParserKtTest {
     fun parse() {
         val scannerResultStr =
             """Start of File,08/01/2022 09:24:26,virtual_volunteer_android_2.1.0_147-HUAWEI_JSN-L21
+,P0003,26/03/2022 09:44:22 AM
 A1000001,P0002,08/01/2022 09:24:26
+A3000005,,26/03/2022 09:44:22 AM
 A2000002,P0001,08/01/2022 09:25:11"""
         val scannerResult = parseScannerResult(scannerResultStr)
         assertIterableEquals(
