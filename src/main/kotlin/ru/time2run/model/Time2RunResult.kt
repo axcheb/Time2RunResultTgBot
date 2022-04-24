89cebf1eb67e0ec01b5f314baa6d3b9403f20162
Aleksey Chebotarev <chebotarev88@gmail.com>
2022-04-24 22:47:48 +0300

    Add UTF BOM

--- src/main/kotlin/ru/time2run/model/Time2RunResult.kt
+++ src/main/kotlin/ru/time2run/model/Time2RunResult.kt
@@ -3,7 +3,8 @@ package ru.time2run.model
 import java.time.LocalTime
 
 private const val UNKNOWN_ATHLETE_NAME = "Неизвестный"
-const val HEADER = "Позиция,Участник,Время\n"
+private const val UTF_BOM = "\uFEFF"
+const val HEADER = UTF_BOM + "Позиция,Участник,Время\n"
 
 data class Time2RunResult(
     val position: Int,
